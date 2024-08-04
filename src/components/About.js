382381ad02f9b734d9b1ad11fc352daa2813d46e
Ivylucyawuor <awuorivy09>
2024-08-04 10:24:08 +0300

    JSX_lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+{/*<div>About</div>;*/}
+<h2>About Me</h2>
+<p>Welcome to JSX lab. We are learning to code</p>
+<img src= {image} alt= "I made this"/>
+</div>
+);
 }
 
 export default About;
